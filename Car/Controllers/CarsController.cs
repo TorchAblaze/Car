80f6da80caefb3fa939d9c9b16039105196d5720
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-11 16:04:18 -0700

    Fix: remove unneccessary code
    Co-authored-by: Ryan Walker <ryandanielwalker@gmail.com>

--- Car/Controllers/CarsController.cs
+++ Car/Controllers/CarsController.cs
@@ -25,7 +25,6 @@ namespace Dealership.Controllers
     public ActionResult Max(int userInput)
     {
       int maxPrice = userInput;
-      string message = "There are no cars available in your price range.";
       Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
       Car yugo = new Car("1980 Yugo Koral", 700, 56000);
       Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
