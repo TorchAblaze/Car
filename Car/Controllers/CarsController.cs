03fdf19c5f816036df00eb61ca1d05f4c5d3d334
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-11 13:46:29 -0700

    Add userinput functinality to website
    Co-authored-by: Ryan Walker <ryandanielwalker@gmail.com>

--- Car/Controllers/CarsController.cs
+++ Car/Controllers/CarsController.cs
@@ -13,7 +13,7 @@ namespace Dealership.Controllers
       Car yugo = new Car("1980 Yugo Koral", 700, 56000);
       Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
       Car amc = new Car("1976 AMC Pacer", 400, 198000);
-      List<Car> allCars = new List<Car>() {volkswagen, yugo, ford, amc};
+      List<Car> allCars = new List<Car>() { volkswagen, yugo, ford, amc };
       return View(allCars);
     }
     [Route("/cars/price")]
@@ -21,10 +21,25 @@ namespace Dealership.Controllers
     {
       return View();
     }
-    // [HttpPost("/cars")]
-    // public ActionResult Create()
-    // {
-    
-    // }
+    [HttpPost("/cars/max")]
+    public ActionResult Max(int userInput)
+    {
+      int maxPrice = userInput;
+      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
+      Car yugo = new Car("1980 Yugo Koral", 700, 56000);
+      Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
+      Car amc = new Car("1976 AMC Pacer", 400, 198000);
+
+      List<Car> allCars = new List<Car>() { volkswagen, yugo, ford, amc };
+      List<Car> CarsMatchingSearch = new List<Car>(0);
+      foreach (Car automobile in allCars)
+      {
+        if (automobile.WorthBuying(maxPrice))
+        {
+          CarsMatchingSearch.Add(automobile);
+        }
+      }
+      return View(CarsMatchingSearch);
+    }
   }
 }
