16510ae454ae12c48e3dafcf7a297ee04ac94815
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-11 11:17:55 -0700

    Add all files for web server cars and form
    Co-authored-by: Ryan Walker <ryandanielwalker@gmail.com>

---
+++ Car/Controllers/CarsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Dealership.Models;
+using System.Collections.Generic;
+
+namespace Dealership.Controllers
+{
+  public class CarsController : Controller
+  {
+    [HttpGet("/cars")]
+    public ActionResult Index()
+    {
+      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
+      Car yugo = new Car("1980 Yugo Koral", 700, 56000);
+      Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
+      Car amc = new Car("1976 AMC Pacer", 400, 198000);
+      List<Car> allCars = new List<Car>() {volkswagen, yugo, ford, amc};
+      return View(allCars);
+    }
+    [Route("/cars/price")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    // [HttpPost("/cars")]
+    // public ActionResult Create()
+    // {
+    
+    // }
+  }
+}
